286d78b1296a311ab3e439add8b65ccbe2837fb9
SanathVenkatachalam <venkatachalam0362@mydusd.org>
2024-10-29 13:30:12 -0700

    2.5.4

--- PhraseSolver.java
+++ PhraseSolver.java
@@ -19,7 +19,8 @@ public class PhraseSolver
   } 
 
   /* your code here - accessor(s) */
-  
+  player1.setName("player 1");
+  player2.setName("player 2");
   /* your code here - mutator(s)  */
 
   public void play()
@@ -32,8 +33,25 @@ public class PhraseSolver
     boolean correct = true;
     while (!solved) 
     {
+      //get a phrase
+      board.loadPhrase();
+      //display name of player
+      System.out.println("Current Player: " + player1.getName());
+      //display player's guessed letter
+      System.out.println("Guessed Letter " + board.guessLetter("a"));
+      //set a Letter value
+      board.setLetterValue();
+      //Display letter value
+      System.out.println("Letter value: " + board.getLetterValue());
+      //display partially solved phrase
+      System.out.println("Current state of phrase: " + board.getPartiallySolvedPhrase());
+      
+
+
+
       
       /* your code here - game logic */
+
       
       
       /* your code here - determine how game ends */
